42b0029dde352fce871c077f6a8c7387f6380697
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2023-08-27 09:38:23 +0300

    OnlyCommentSchema added

--- schema/commentschema.py
+++ schema/commentschema.py
@@ -4,14 +4,28 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import validates, post_load, post_dump, fields
 
 
-
-class CommentSchema(SQLAlchemyAutoSchema):
+class OnlyCommentSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Comment
+        include_relationships = True
+        include_fk = True
+        include_relationships = False
+    product_id = fields.Integer(required=True)
+    
+    @post_load
+    def create_comment(self, data,**kwargs)-> Comment:
+        return Comment(**data)
+    
+    
+    
+class CommentSchema(OnlyCommentSchema):
     class Meta:
    
         model = Comment
         include_relationships = True
         include_fk = True
     product = fields.Nested("ProductSchema",exclude=("comments",))
-    @post_load
-    def create_comment(self, data,**kwargs)-> Comment:
-        return Comment(**data)
+    
+
+    
+
